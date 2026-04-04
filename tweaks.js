c011c74df51fcea7dafc43a9a7ed72017bd8bf9a
shmitt7 <rahachevich@gmail.com>
2026-04-04 10:33:57 +0300

    Update tweaks.js

--- tweaks.js
+++ tweaks.js
@@ -1,1 +1,85 @@
-(function(){'use strict';if(!window.liteTweaks)window.liteTweaks={};Object.assign(window.lampa_settings,{disable_features:{dmca:!0,ai:!0,subscribe:!0,blacklist:!0,persons:!0,ads:!0,install_proxy:!0},feed:!1,services:!1,geo:!1,lang_use:!1,dcma:!1});window.plugin_shots_ready=!0;window.lampa_settings.disable_features.lgbt=!0;function run(){Lampa.AdManager?.destroy(),$('.ad-video-block, [class*="ad-"], .ad_plugin').remove();if(Lampa.Player?.play){const e=Lampa.Player.play;Lampa.Player.play=function(t){return t.iptv=!0,delete t.vast_url,delete t.vast_msg,e.call(this,t)}}Lampa.Listener.follow('full',e=>{e.type==='complite'&&$('.button--options').remove()});$('.head__action.open--notice, .head__action.notice--icon').remove();if(Lampa.Notice?.drawCount)Lampa.Notice.drawCount=()=>{};$('.head__action.open--premium, .head__action.open--feed, .head__action.open--broadcast, .black-friday__button').remove();const e=()=>{$('.watched-history').remove();const e=$('.torrent-item').first();e.length&&Lampa.Controller.collectionFocus(e,$('.scroll').first())};e(),new MutationObserver(e).observe(document.body,{childList:!0,subtree:!0});$('body').append('<style>.torrent-item__seeds span,.torrent-item__grabs span{font-weight:800;font-size:1.25em}</style>')}window.appready?run():Lampa.Listener.follow('app',e=>{e.type==='ready'&&run()})})();
+(function() {  
+    'use strict';  
+  
+    if (!window.liteTweaks) {  
+        window.liteTweaks = {};  
+    }  
+  
+    Object.assign(window.lampa_settings, {  
+        disable_features: {  
+            dmca: true,  
+            ai: true,  
+            subscribe: true,  
+            blacklist: true,  
+            persons: true,  
+            ads: true,  
+            install_proxy: true  
+        },  
+        feed: false,  
+        services: false,  
+        geo: false,  
+        lang_use: false,  
+        dcma: false  
+    });  
+  
+    window.plugin_shots_ready = true;  
+    window.lampa_settings.disable_features.lgbt = true;  
+    window.lampa_settings.torrents_use = true;  
+    window.lampa_settings.demo = false;  
+    window.lampa_settings.read_only = false;  
+  
+    function run() {  
+        Lampa.AdManager?.destroy();  
+        $('.ad-video-block, [class*="ad-"], .ad_plugin').remove();  
+  
+        if (Lampa.Player?.play) {  
+            const originalPlay = Lampa.Player.play;  
+            Lampa.Player.play = function(data) {  
+                data.iptv = true;  
+                delete data.vast_url;  
+                delete data.vast_msg;  
+                return originalPlay.call(this, data);  
+            };  
+        }  
+  
+        Lampa.Listener.follow('full', (e) => {  
+            if (e.type === 'complite') {  
+                $('.button--options').remove();  
+            }  
+        });  
+  
+        $('.head__action.open--notice, .head__action.notice--icon').remove();  
+  
+        if (Lampa.Notice?.drawCount) {  
+            Lampa.Notice.drawCount = () => {};  
+        }  
+  
+        $('.head__action.open--premium, .head__action.open--feed, .head__action.open--broadcast, .black-friday__button').remove();  
+  
+        const removeHistory = () => {  
+            $('.watched-history').remove();  
+            const firstTorrent = $('.torrent-item').first();  
+            if (firstTorrent.length) {  
+                Lampa.Controller.collectionFocus(firstTorrent, $('.scroll').first());  
+            }  
+        };  
+  
+        removeHistory();  
+        new MutationObserver(removeHistory).observe(document.body, {  
+            childList: true,  
+            subtree: true  
+        });  
+  
+        $('body').append('<style>.torrent-item__seeds span,.torrent-item__grabs span{font-weight:800;font-size:1.25em}</style>');  
+    }  
+  
+    if (window.appready) {  
+        run();  
+    } else {  
+        Lampa.Listener.follow('app', (e) => {  
+            if (e.type === 'ready') {  
+                run();  
+            }  
+        });  
+    }  
+})();
