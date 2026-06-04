830294b87976b3d40346f3dc15048493c9522060
shmitt7 <rahachevich@gmail.com>
2026-06-04 09:07:14 +0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -11,7 +11,8 @@
             subscribe: true,  
             blacklist: true,  
             persons: true,  
-            ads: true,    
+            ads: true,  
+            install_proxy: true,  
             remote_configuration: true,  
         },  
         torrents_use: true,  
@@ -20,7 +21,8 @@
         lang_use: false,  
         white_use: false,  
         read_only: false,  
-        demo: false,    
+        demo: false,  
+        dcma: false,  
     });  
     window.plugin_shots_ready = true;  
   
@@ -29,10 +31,22 @@
         window.liteTweaks._started = true;  
   
         // === БЛОК 2: Удаление кнопок ===  
+        // Шапка  
         $('.head__action.open--notice, .head__action.notice--icon').remove();  
         $('.head__action.open--premium, .head__action.open--feed, .head__action.open--broadcast, .head__action.open--profile').remove();  
         $('.black-friday__button').remove();  
         if (Lampa.Notice?.drawCount) Lampa.Notice.drawCount = () => {};  
+  
+        // Меню: убираем ненужные пункты  
+        $('.menu [data-action="catalog"], .menu [data-action="relise"], .menu [data-action="timetable"], .menu [data-action="mytorrents"], .menu [data-action="about"]').remove();  
+        // Перемещаем "Редактировать" в блок с настройками и убираем пустой блок  
+        const editItem = $('.menu [data-action="edit"]').detach();  
+        $('.menu .nosort:first .menu__list').append(editItem);  
+        const lastNosort = $('.menu .nosort:last');  
+        lastNosort.prev('.menu__split').remove();  
+        lastNosort.remove();  
+  
+        // Кнопка "Опции" в карточке  
         Lampa.Listener.follow('full', (e) => {  
             if (e.type === 'complite') {  
                 e.object.activity.render().find('.button--options').remove();  
