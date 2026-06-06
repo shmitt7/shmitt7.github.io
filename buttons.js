6b62042e424c32e4f97b7446e6bfc3c4b74f686d
shmitt7 <rahachevich@gmail.com>
2026-06-06 08:37:03 +0300

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -19,8 +19,8 @@
         BUTTONS.forEach(({selector, svg}) => $container.find(selector).html(svg));  
         const $moved = $container.children();  
         $moved.prependTo($target);  
-        if (e.link && e.link.items && e.link.items[0]) {  
-            const start = e.link.items[0];  
+        const start = e.link?.items?.[0];  
+        if (start) {  
             $moved.filter('.selector').on('hover:focus hover:enter hover:hover hover:touch', (ev) => {  
                 start.last = ev.target;  
             });  
