76612dac1e9fec89672cd7bac1c3be86a7be1cbe
shmitt7 <rahachevich@gmail.com>
2026-07-26 11:53:54 +0300

    Update 9.js

--- 9.js
+++ 9.js
@@ -5,7 +5,7 @@
         '.ccs__bar {',
             'position: absolute;',
             'bottom: 0; left: 0; right: 0;',
-            'padding: 1.8em 0.6em 1em 0.6em;',
+            'padding: 1.8em 0.6em 0.3em 0.6em;',
             'background: linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.88) 100%);',
             'border-bottom-left-radius: 1em;',
             'border-bottom-right-radius: 1em;',
