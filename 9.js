196ee1dcd95154bf72429ec0a3979df931cbec5a
shmitt7 <rahachevich@gmail.com>
2026-08-20 22:39:04 +0300

    Update 9.js

--- 9.js
+++ 9.js
@@ -1,22 +1,20 @@
 (function () {  
-    console.log('[dmca-debug] script loaded, disable_features.dmca =', window.lampa_settings && window.lampa_settings.disable_features && window.lampa_settings.disable_features.dmca)  
-    console.log('[dmca-debug] window.lampa_settings.dcma at load =', window.lampa_settings && window.lampa_settings.dcma)  
+    console.log('[dmca-debug] script loaded at', Date.now())  
+    console.log('[dmca-debug] disable_features.dmca =', window.lampa_settings && window.lampa_settings.disable_features && window.lampa_settings.disable_features.dmca)  
+    console.log('[dmca-debug] dcma list length at load =', (window.lampa_settings && window.lampa_settings.dcma || []).length)  
   
-    Lampa.Listener.follow('request_before', function (e) {  
-        if (/\/(movie|tv)\/\d+/.test(e.params.url)) {  
-            console.log('[dmca-debug] request_before:', e.params.url)  
-        }  
-    })  
-  
-    Lampa.Listener.follow('request_secuses', function (e) {  
-        if (/\/(movie|tv)\/\d+/.test((e.params && e.params.url) || '')) {  
-            console.log('[dmca-debug] request_secuses:', e.params.url, 'blocked =', e.data && e.data.blocked)  
-        }  
-    })  
-  
-    Lampa.Listener.follow('request_error', function (e) {  
-        if (/\/(movie|tv)\/\d+/.test((e.params && e.params.url) || '')) {  
-            console.log('[dmca-debug] request_error:', e.params.url, e.error && e.error.status)  
-        }  
-    })  
+    // следим за КАЖДОЙ записью dcma, чтобы поймать момент, когда список наполняется  
+    try {  
+        var _dcma = window.lampa_settings.dcma  
+        Object.defineProperty(window.lampa_settings, 'dcma', {  
+            get: function () { return _dcma },  
+            set: function (val) {  
+                console.log('[dmca-debug] window.lampa_settings.dcma OVERWRITTEN, new length =', val && val.length, 'stack:', new Error().stack)  
+                _dcma = val  
+            },  
+            configurable: true  
+        })  
+    } catch (e) {  
+        console.warn('[dmca-debug] cannot hook dcma setter', e)  
+    }  
 })();
