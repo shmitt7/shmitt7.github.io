3a5783a475ffab26ae6bcd6ff2a46db59cf12a14
shmitt7 <rahachevich@gmail.com>
2026-08-21 14:44:25 +0300

    Update 9.js

--- 9.js
+++ 9.js
@@ -1,4 +1,14 @@
-(function () {  
-    'use strict';  
-    // пусто  
-})();
+function startMyPlugin() {  
+    window.my_plugin_ready = true  
+  
+    function init() {  
+        // здесь будет код плагина  
+    }  
+  
+    if (window.appready) init()  
+    else Lampa.Listener.follow('app', function (e) {  
+        if (e.type == 'ready') init()  
+    })  
+}  
+  
+if (!window.my_plugin_ready) startMyPlugin()
