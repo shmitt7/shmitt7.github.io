d9b750217ec3cd92be1b17454b2015836af36ae0
shmitt7 <rahachevich@gmail.com>
2026-08-28 11:05:23 +0300

    Update torrserver.js

--- torrserver.js
+++ torrserver.js
@@ -1,33 +1,55 @@
-(function() {  
-    if (window.switchTorrServer) return;  
+(function(){  
+    if(window.switchTorrServer) return;  
     window.switchTorrServer = true;  
-    document.head.insertAdjacentHTML('beforeend', '<style>.head__action.switch-server.focus img,.head__action.switch-server.hover img{-webkit-filter:brightness(0.3);filter:brightness(0.3)}</style>');  
-    var switchServer = function() {  
-        var xhr = new XMLHttpRequest();  
-        xhr.open('GET', 'http://185.87.48.42:8090/random_torr');  
-        xhr.onload = function() {  
-            var ip = xhr.status === 200 ? xhr.responseText.trim() : '';  
-            if (ip) {  
+    var network = new Lampa.Reguest();  
+    var button;  
+    var styleAdded = false;  
+    function addStyle(){  
+        if(styleAdded) return;  
+        styleAdded = true;  
+        document.head.insertAdjacentHTML('beforeend', '<style>.head__action.switch-server.focus img,.head__action.switch-server.hover img{-webkit-filter:brightness(0.3);filter:brightness(0.3)}</style>');  
+    }  
+    function switchServer(){  
+        network.silent('http://185.87.48.42:8090/random_torr', function(response){  
+            var ip = response ? String(response).trim() : '';  
+            if(ip){  
                 Lampa.Storage.set('torrserver_url_two', 'http://' + ip + ':8090');  
                 Lampa.Noty.show('TorrServer изменён http://' + ip + ':8090');  
-            } else {  
-                Lampa.Noty.show('TorrServer недоступен');  
             }  
-        };  
-        xhr.onerror = function() { Lampa.Noty.show('TorrServer недоступен'); };  
-        xhr.send();  
-    };  
-    var addButton = function() {  
-        if (!Lampa.Storage.field('internal_torrclient') || Lampa.Storage.get('torrserver_use_link') !== 'two') return;  
-        var button = Lampa.Head.addIcon('<img src="./img/icons/settings/server.svg" />', switchServer).addClass('switch-server');  
-        Lampa.Listener.follow('activity', function(e) {  
-            if (e.type !== 'start') return;  
-            button.toggle(e.component === 'torrents');  
-            if (e.component === 'torrents') switchServer();  
+            else Lampa.Noty.show('TorrServer недоступен');  
+        }, function(){  
+            Lampa.Noty.show('TorrServer недоступен');  
+        }, false, {  
+            timeout: 5000,  
+            dataType: 'text'  
         });  
-        var _active = Lampa.Activity.active();  
-        button.toggle(_active && _active.component === 'torrents');  
-    };  
-    if (window.appready) addButton();  
-    else Lampa.Listener.follow('app', function(e) { if (e.type === 'ready') addButton(); });  
+    }  
+    function isTorrentsActive(){  
+        var active = Lampa.Activity.active();  
+        return !!(active && active.component === 'torrents');  
+    }  
+    function canShowButton(){  
+        return !!Lampa.Storage.field('internal_torrclient') && Lampa.Storage.get('torrserver_use_link') === 'two';  
+    }  
+    function updateVisibility(){  
+        if(!button) return;  
+        var show = canShowButton() && isTorrentsActive();  
+        button.toggle(show);  
+    }  
+    function addButton(){  
+        if(!canShowButton()) return;  
+        addStyle();  
+        button = Lampa.Head.addIcon('<img src="./img/icons/settings/server.svg" />', switchServer).addClass('switch-server');  
+        Lampa.Listener.follow('activity', function(e){  
+            if(e.type !== 'start') return;  
+            updateVisibility();  
+            if(isTorrentsActive()) switchServer();  
+        });  
+        Lampa.Storage.listener.follow('change', function(e){  
+            if(e.name === 'internal_torrclient' || e.name === 'torrserver_use_link') updateVisibility();  
+        });  
+        updateVisibility();  
+    }  
+    if(window.appready) addButton();  
+    else Lampa.Listener.follow('app', function(e){ if(e.type === 'ready') addButton(); });  
 })();
