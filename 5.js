e6e0764e4dc7ffd42e0138351f0e162bebe92d8a
shmitt7 <rahachevich@gmail.com>
2026-09-20 18:33:02 +0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,14 +1,14 @@
-function startMyPlugin() {  
-    window.my_plugin_ready = true  
-  
-    function init() {  
-        // здесь будет код плагина  
-    }  
-  
-    if (window.appready) init()  
-    else Lampa.Listener.follow('app', function (e) {  
-        if (e.type == 'ready') init()  
+(function () {  
+    Lampa.Listener.follow('request_secuses', function (e) {  
+        if (e.params.url && e.params.url.indexOf('kinopoisk') >= 0) {  
+            console.log('KP_DUMP_URL:', e.params.url)  
+            console.log('KP_DUMP_DATA:', JSON.stringify(e.data).slice(0, 2000))  
+        }  
     })  
-}  
   
-if (!window.my_plugin_ready) startMyPlugin()
+    Lampa.Listener.follow('request_error', function (e) {  
+        if (e.params.url && e.params.url.indexOf('kinopoisk') >= 0) {  
+            console.log('KP_DUMP_ERROR_URL:', e.params.url)  
+        }  
+    })  
+})()
